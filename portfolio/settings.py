a0885100f05ad575f8b59eef5f99841e7e80c7f3
TusharPanchal55 <tpanchal827@gmail.com>
2026-01-01 14:14:25 +0530

    Fixed Deployment Issue

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mxqvh&rlgqj!f^kz%(u&7^@_gdr%fd74q)p$kuh2n7kh*hcd2-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['portfolio-z841.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
